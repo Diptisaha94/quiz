78e17dab8ec6dd6f231ebaaecfa713934c23368d
Dipti saha <sahadipti322@gmail.com>
2022-10-11 10:00:30 -0400

    header part responsive

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import { faXmark,faBars } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 //import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return (
+    const [open,setOpen]=useState(false);
+        return (
         <div className='flex justify-between items-center px-20 py-6 bg-cyan-200'>
             <div className="text-2xl font-semibold">Play Quiz</div>
-                <ul className='flex'>
+            <div onClick={()=>setOpen(!open)} className="h-6 w-6 md:hidden">
+                {
+                    open?<FontAwesomeIcon icon={faXmark}/>:<FontAwesomeIcon icon={faBars}/>
+                }
+            </div>
+                <ul className={`md:flex absolute md:static sm:hidden ${open?'top-16 pl-4 py-4 left-14 bg-cyan-300':'top-[-120px]'}`}>
                 <li className='pr-10'>
                         <NavLink to="/home" className={({isActive})=>
                     isActive?"text-indigo-500":"text-black"
